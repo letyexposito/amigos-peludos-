154b902445737c5bb03615780089cc0eb01b2876
letyexposito <nallelyexposito259@gmail.com>
2025-01-08 13:54:48 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,23 @@
 
+let miBoton = document.querySelector('#changeUser');
+let miEncabezado = document.querySelector('h1 span');
+
+function establecerNombre() {
+    let miNombre = prompt('Por favor, ingresa tu nombre:');
+    if (!miNombre || miNombre === null) {
+        miNombre = 'Amante de Mascotas';
+    }
+    localStorage.setItem('nombre', miNombre);
+    miEncabezado.textContent = miNombre;
+}
+
+if (!localStorage.getItem('nombre')) {
+    establecerNombre();
+} else {
+    let nombreAlmacenado = localStorage.getItem('nombre');
+    miEncabezado.textContent = nombreAlmacenado;
+}
+
+miBoton.onclick = function () {
+    establecerNombre();
+};
